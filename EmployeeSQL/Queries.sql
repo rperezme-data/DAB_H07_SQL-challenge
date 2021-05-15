697ceb9ff1d4445b04212d89695ade60bf4a7c0d
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-05-15 00:46:51 -0500

    Add Query: Hercules B.

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -14,7 +14,7 @@ SET datestyle = 'ISO,MDY';
 
 SELECT TO_DATE('7/25/1953','MM/DD/YYYY');
 
-
+-- 1. Employee Details
 SELECT
 	e.emp_no,
 	e.last_name,
@@ -25,3 +25,22 @@ FROM employee AS e
 JOIN salary AS s
 ON (e.emp_no = s.emp_no)
 ORDER BY e.emp_no;
+
+
+
+-- 5. Employees first name "Hercules" & last name begin with "B"
+SELECT
+	e.first_name,
+	e.last_name,
+	e.sex
+FROM employee AS e
+WHERE
+	e.first_name = 'Hercules' AND
+	e.last_name LIKE 'B%'
+ORDER BY e.last_name;
+
+
+
+
+
+
