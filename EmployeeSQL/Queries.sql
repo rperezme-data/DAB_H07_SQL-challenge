e017f57ae3dd115d206942e3870eda64f0c75d4e
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-05-17 01:14:55 -0500

    Add Query: Department per employee

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -25,7 +25,7 @@ WHERE EXTRACT(YEAR FROM TO_DATE(e.birth_date,'MM/DD/YYYY')) = 1957;
 
 
 
--- 1. Employee Details
+-- 1. Employees Details
 SELECT
 	e.emp_no,
 	e.last_name,
@@ -48,7 +48,7 @@ WHERE EXTRACT(YEAR FROM TO_DATE(e.hire_date,'MM/DD/YYYY')) = 1986
 ORDER BY e.hire_date;
 
 
--- 3. Manager per department 
+-- 3. Managers per department 
 SELECT
 	dm.dept_id,
 	d.dept_name,
@@ -63,7 +63,21 @@ ON dm.emp_no = e.emp_no
 ORDER BY dm.dept_id;
 
 
--- 5. Employees first name "Hercules" & last name begin with "B"
+-- 4. Department per employee
+SELECT
+	e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+FROM dept_employee AS de
+LEFT JOIN employee AS e
+ON de.emp_no = e.emp_no
+LEFT JOIN department AS d
+ON de.dept_id = d.dept_id
+ORDER BY e.emp_no;
+
+
+-- 5. Employees "Hercules B..."
 SELECT
 	e.first_name,
 	e.last_name,
@@ -72,7 +86,7 @@ FROM employee AS e
 WHERE
 	e.first_name = 'Hercules' AND
 	e.last_name LIKE 'B%'
-ORDER BY e.last_name;
+ORDER BY e.first_name, e.last_name;
 
 
 
