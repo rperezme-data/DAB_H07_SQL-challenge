a8ff438e6f2ee31d8eeb455320cece6a3b3bf666
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-05-18 18:29:21 -0500

    Update Schema

--- EmployeeSQL/Schema.sql
+++ EmployeeSQL/Schema.sql
@@ -1,22 +1,22 @@
 -- a) Drop existing tables
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS title;
-DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS dept_employee;
-DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS salary;
+DROP TABLE IF EXISTS department CASCADE;
+DROP TABLE IF EXISTS title CASCADE;
+DROP TABLE IF EXISTS employee CASCADE;
+DROP TABLE IF EXISTS dept_employee CASCADE;
+DROP TABLE IF EXISTS dept_manager CASCADE;
+DROP TABLE IF EXISTS salary CASCADE;
 
 
 -- TABLE SCHEMA:
 -- Create new tables
 CREATE TABLE department (
-	dept_id CHAR(5) NOT NULL, -- CHAR(4)
+	dept_id VARCHAR(5) NOT NULL, -- CHAR(4)
 	PRIMARY KEY (dept_id),
 	dept_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE title (
-	title_id CHAR(5) NOT NULL,
+	title_id VARCHAR(5) NOT NULL,
 	PRIMARY KEY (title_id),
 	title_name VARCHAR(50) NOT NULL
 );
@@ -24,25 +24,25 @@ CREATE TABLE title (
 CREATE TABLE employee (
 	emp_no INT NOT NULL,
 	PRIMARY KEY (emp_no),
-	title_id CHAR(5) NOT NULL,
+	title_id VARCHAR(5) NOT NULL,
 	FOREIGN KEY (title_id) REFERENCES title(title_id),
 	birth_date VARCHAR(10) NOT NULL, --DATE
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
-	sex CHAR(1) NOT NULL, --Test
+	sex VARCHAR(5) NOT NULL, --CHAR(1)
 	hire_date VARCHAR(10) NOT NULL --DATE
 );
 
 CREATE TABLE dept_employee (
 	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employee(emp_no),
-	dept_id CHAR(5) NOT NULL,
+	dept_id VARCHAR(5) NOT NULL,
 	FOREIGN KEY (dept_id) REFERENCES department(dept_id),
 	PRIMARY KEY (emp_no, dept_id)
 );
 
 CREATE TABLE dept_manager (
-	dept_id CHAR(5) NOT NULL,
+	dept_id VARCHAR(5) NOT NULL,
 	FOREIGN KEY (dept_id) REFERENCES department(dept_id),
 	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employee(emp_no),
@@ -52,7 +52,7 @@ CREATE TABLE dept_manager (
 CREATE TABLE salary (
 	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employee(emp_no),
-	salary money NOT NULL,
+	salary INT NOT NULL, --MONEY
 	PRIMARY KEY (emp_no, salary)
 );
 
